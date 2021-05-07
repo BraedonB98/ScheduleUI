c9567d29aa30ad2f7bcfed852f091cb770400aa8
braedonb98 <brbe3574@colorado.edu>
2021-05-06 18:49:06 -0600

    console logging profile on dash board

--- Pages/HomePage/Homepage.js
+++ Pages/HomePage/Homepage.js
@@ -2,6 +2,7 @@ var memStorage = window.sessionStorage;
 var locStorage = window.localStorage;
 
 var profile = JSON.parse(memStorage.getItem("googleUser"));
+console.log (profile);
 
 console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
 console.log('Name: ' + profile.getName());
