36aa9bff96c887eb56d816dc17afd16b016f79c4
braedonb98 <brbe3574@colorado.edu>
2021-05-06 18:51:03 -0600

    console logging profile, removing console logs from ID

--- Pages/HomePage/Homepage.js
+++ Pages/HomePage/Homepage.js
@@ -4,7 +4,7 @@ var locStorage = window.localStorage;
 var profile = JSON.parse(memStorage.getItem("googleUser"));
 console.log (profile);
 
-console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-console.log('Name: ' + profile.getName());
-console.log('Image URL: ' + profile.getImageUrl());
-console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//console.log('Name: ' + profile.getName());
+//console.log('Image URL: ' + profile.getImageUrl());
+//console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
