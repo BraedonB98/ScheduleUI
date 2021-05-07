3afa4c48599233af5e0c1b61a835564a9903f57b
braedonb98 <brbe3574@colorado.edu>
2021-05-06 19:01:16 -0600

    testing if things are saving to memstorage correct

--- Pages/HomePage/Homepage.js
+++ Pages/HomePage/Homepage.js
@@ -3,7 +3,15 @@ var locStorage = window.localStorage;
 
 var profile = JSON.parse(memStorage.getItem("googleUser"));
 console.log (profile);
-
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    memStorage.setItem('googleUser',JSON.stringify(profile));
+    
+  }
 console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
 console.log('Name: ' + profile.getName());
 console.log('Image URL: ' + profile.getImageUrl());
