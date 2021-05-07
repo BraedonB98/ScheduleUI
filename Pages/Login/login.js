e972cefb93b6d79ca126d66f1300725f62d7b042
braedonb98 <brbe3574@colorado.edu>
2021-05-06 19:07:59 -0600

    try to access user data a different way

--- Pages/Login/login.js
+++ Pages/Login/login.js
@@ -75,7 +75,5 @@ function onSignIn(googleUser) {
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    memStorage.setItem('googleUser',JSON.stringify(profile));
-    
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.    
   }
