09faa96249e6cdeaeb13ec31b7019328a12d72d3
braedonb98 <brbe3574@colorado.edu>
2021-05-07 19:37:46 -0600

    parsing stringified user data

--- Pages/HomePage/Homepage.js
+++ Pages/HomePage/Homepage.js
@@ -1,11 +1,13 @@
 
 var memStorage = window.sessionStorage;
 var locStorage = window.localStorage;
-
+var userInfo;
 function defaultPage()
 {
     if(memStorage.getItem('user')!=null)
     {
+        userInfo = JSON.parse(memStorage.getItem('user'));
+        console.log(userInfo.username)
         console.log(memStorage.getItem('user'));
         document.getElementById('content').innerHTML = "<H1>Welcome User<H1>";
     }
