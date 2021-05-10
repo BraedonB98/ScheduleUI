9d77d3d68124ecc56e2f4a677a55ba3dcc5e5588
braedonb98 <brbe3574@colorado.edu>
2021-05-10 14:49:46 -0600

    adding wecome followed by users name on homepage

--- Pages/HomePage/Homepage.js
+++ Pages/HomePage/Homepage.js
@@ -9,7 +9,7 @@ function defaultPage()
         userInfo = JSON.parse(memStorage.getItem('user'));
         console.log(userInfo.username)
         console.log(memStorage.getItem('user'));
-        document.getElementById('content').innerHTML = "<H1>Welcome User<H1>";
+        document.getElementById('content').innerHTML = `<H1>Welcome ${userInfo.username}<H1>`;
     }
     else{
         console.log("need to sign in")
